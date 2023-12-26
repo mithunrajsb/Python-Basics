027ba29dd5f3af009a380b11c0642f690fb10cb5
mithunrajsb <mrsb@live.in>
2023-12-26 09:21:48 +0530

    Update pandas_pgm1.py

--- pandas/pandas_pgm1.py
+++ pandas/pandas_pgm1.py
@@ -1,1 +1,16 @@
+import pandas as pd
 
+# Creating a DataFrame from a dictionary
+data = {'Name': ['Anil', 'Bobby', 'Charlie'],
+        'Age': [25, 30, 35],
+        'City': ['New Delhi', 'Chicago', 'London']}
+
+df = pd.DataFrame(data)
+
+# Displaying specific columns
+print(df['Name'])  # Display 'Name' column
+
+# Filtering data
+filtered_data = df[df['Age'] > 25]  # Get rows where Age > 25
+
+print(filtered_data)
