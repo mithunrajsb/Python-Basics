5957472e1c2fa815c7bb7f29597cf3ddb9e6ad56
mithunrajsb <mrsb@live.in>
2023-12-31 15:26:37 +0530

    adding OOP, Exception and else

---
+++ Exceptions/exc_pgm5.py
@@ -0,0 +1,11 @@
+# Exception handling demo
+# using else and finally
+
+try:
+    n = int(input("Enter an integer:"))
+    result = int(100/n)
+except:
+    print("Sorry..Unable to calculate..")
+else:
+    print("100/{} = {}".format(n, result))
+print("exiting the program")
