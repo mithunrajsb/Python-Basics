f3601337420eccb46dadeacc70e9edbad80ebc93
mithunrajsb <mrsb@live.in>
2023-12-31 15:34:43 +0530

    adding OOP, Nested try blocks

---
+++ Exceptions/exc_pgm7.py
@@ -0,0 +1,18 @@
+# Raising exceptions
+# Nested try blocks
+
+try:
+    try:
+
+        n=int(input("Enter an integer :"))
+        if n>100: raise ValueError()
+        result = int(100/n)
+        print("100/{} = {}".format(n, result))
+
+    except ValueError:
+        print("Please enter an integer less than 100!")
+
+except ZeroDivisionError:
+    print("Sorry! Division by zero is not permitted!")
+
+print("Thanks for using this program")
