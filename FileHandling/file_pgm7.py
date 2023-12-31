fe5c4656e79783406876ab09495c7d4bd8d7d252
mithunrajsb <mrsb@live.in>
2023-12-31 17:52:38 +0530

    file handling reading first and last 10 lines

--- FileHandling/file_pgm7.py
+++ FileHandling/file_pgm7.py
@@ -1,10 +1,12 @@
 # Displaying the first 10 & last 10 lines of a file 
 fileName = "factorials.txt"
-start = 
+start = int(input("Enter starting line:"))
+length = int(input("Enter number of lines:"))
 
 try:
     file=open(fileName)
     lines = file.readlines()
+    lines = lines[start-1:start+length -1]
     if len(lines)>10:lines = lines[-10:]
     print("".join(lines),end='')
     
