ac03e18ca8ae0564891674911f70862175465ac9
mithunrajsb <mrsb@live.in>
2023-12-31 17:46:56 +0530

    file handling reading last 10 lines

--- FileHandling/file_pgm6.py
+++ FileHandling/file_pgm6.py
@@ -1,12 +1,12 @@
-# Displaying the first 10 lines of a file using head command
+# Displaying the last 10 lines of a file using head command
 fileName = "factorials.txt"
 
 try:
     file=open(fileName)
-    for i in range(10):
-        data = file.readline()
-        if not data: break
-        print(data, end='')
+    lines = file.readlines()
+    if len(lines)>10:lines = lines[-10:]
+    print("".join(lines),end='')
+    
 except Exception as e:
     print("Unable to open file: {}".format(fileName))
     print("Reason: {}".format(str(e)))
